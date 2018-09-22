9305e2ddd4c0632ba091071bfc86cdbd12b98225
AlexKuchinsky <alexkuchinskydev@gmail.com>
2018-09-22 19:56:00 +0300

    Improved work with permissions

--- App4U-0.1.2/app/src/main/java/com/example/alex/app4u/MainActivity.kt
+++ App4U-0.1.2/app/src/main/java/com/example/alex/app4u/MainActivity.kt
@@ -12,6 +12,10 @@ import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.support.design.widget.Snackbar;
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imeiTextView: TextView
     private lateinit var versionTextView: TextView
     private lateinit var showIMEIButton: Button
+    private lateinit var mainActivityLayout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         showIMEIButton = findViewById(R.id.showIMEIButton)
         imeiTextView = findViewById(R.id.IMEITextView)
         versionTextView = findViewById(R.id.versionTextView)
+        mainActivityLayout = findViewById(R.id.mainActivityLayout)
 
         try {
             val pInfo = getPackageManager().getPackageInfo(packageName, 0)
@@ -47,23 +53,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun askForReadPhoneStatePermission(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.READ_PHONE_STATE)) {
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_PHONE_STATE),
-                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_PHONE_STATE),
-                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-            }
-        }
-    }
-
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
@@ -72,8 +61,16 @@ class MainActivity : AppCompatActivity() {
                     showIMEIButton.visibility = View.GONE
                     imeiTextView.text = getString(R.string.your_imei) + getIMEI()
                 } else {
-                    Toast.makeText(this, "You have to give permission to see the IMEI",
-                            Toast.LENGTH_LONG).show()
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                    Manifest.permission.READ_PHONE_STATE)){
+                        Toast.makeText(this, "You have to give permission to see the IMEI",
+                                Toast.LENGTH_LONG).show()
+                    }else{
+                        Snackbar.make(mainActivityLayout, "You have to give permission manually",
+                                Snackbar.LENGTH_LONG)
+                                .setAction("SETTINGS", {openSettingsForPhoneStatePermission()})
+                                .show()
+                    }
                 }
                 return
             }
@@ -81,6 +78,37 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    private fun askForReadPhoneStatePermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_PHONE_STATE)) {
+                val message = "Permission is needed to use this functionality"
+                Snackbar.make(mainActivityLayout, message, Snackbar.LENGTH_INDEFINITE)
+                        .setAction("GRANT", View.OnClickListener {
+                            ActivityCompat.requestPermissions(this,
+                                    arrayOf(Manifest.permission.READ_PHONE_STATE),
+                                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+                        })
+                        .show()
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_PHONE_STATE),
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+            }
+        }
+    }
+
+    private fun openSettingsForPhoneStatePermission(){
+        val appSettingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:$packageName"))
+        startActivityForResult(appSettingsIntent, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+
+        finish();
+        startActivity(getIntent());
     }
 
     private fun getIMEI(): String {
